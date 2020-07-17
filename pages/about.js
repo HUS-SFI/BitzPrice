c73a0dcf1f941a5c500d92ef40f8b717e519c46f
Hossein Seifi <hossein.seifi.98@gmail.com>
2020-07-17 20:10:23 +0430

    style fixes

--- pages/about.js
+++ pages/about.js
@@ -7,8 +7,23 @@ const About = () => {
                 <title>About</title>
             </Head>
             <div className="about">
-                <p> An application to view prices</p>
+                <h1> wlcome </h1>
+                <p>
+                    it's a web application that shows Bitcoin's price,
+                    <br />
+                    developed with Next.js by "HUS-SFI"
+                </p>
             </div>
+            <style jsx>{`
+                .about {
+                    background-color: #f4f4f4;
+                    border: 1px solid #ccc;
+                    max-width: 400px;
+                    padding: 1rem 5rem;
+                    margin: auto;
+                    margin-top: 1rem;
+                }
+            `}</style>
         </Layout>
     )
 }
