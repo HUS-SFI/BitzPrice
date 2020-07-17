0bfe8e488938ec0cd015199f2a2cc8d84b717027
Hossein Seifi <hossein.seifi.98@gmail.com>
2020-07-17 16:17:54 +0430

    setup

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header"
+
+const About = () => {
+    return (
+        <div className="about">
+            <Header />
+            <p> An application to view prices</p>
+        </div>
+    )
+}
+
+export default About
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+import Header from "../components/Header"
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Welcome to bitPrize</h1>
+        </div>
+    )
+}
+
+export default Home
